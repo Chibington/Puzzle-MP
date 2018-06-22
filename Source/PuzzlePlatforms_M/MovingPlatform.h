173abd9d93506ff17f9901fc5105cb28599cf2fc
DESKTOP-L4VMUFS\Chris <chrisjokim@gmail.com>
2018-06-21 23:35:43 -0400

    Tested Server-Client relationship and added moving platform with Authoritative logic

---
+++ Source/PuzzlePlatforms_M/MovingPlatform.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/StaticMeshActor.h"
+#include "MovingPlatform.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUZZLEPLATFORMS_M_API AMovingPlatform : public AStaticMeshActor
+{
+	GENERATED_BODY()
+
+public:
+	AMovingPlatform();
+
+	UPROPERTY(EditAnywhere)
+		float speed = 20;
+
+protected:
+
+	virtual void Tick(float DeltaTime) override;
+
+
+};
